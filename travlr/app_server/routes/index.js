3a4b513c024a1a46191089d3100be046e1e9c503
Colin-Kapsner <colinkapsner@yahoo.com>
2026-07-10 02:25:12 -0600

    Module 2 completed baseline

--- travlr/app_server/routes/index.js
+++ travlr/app_server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
+
+router.get('/', ctrlMain.index);
+
+/*
+This was the original router.get call
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+*/
 module.exports = router;
