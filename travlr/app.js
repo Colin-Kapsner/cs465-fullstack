9c2f544c0ffb24a0a6aaac908ea2f9191c95614f
Colin-Kapsner <colinkapsner@yahoo.com>
2026-07-18 18:25:31 -0600

    Module 3 completed baseline

--- travlr/app.js
+++ travlr/app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var handlebars = require('hbs');
-
 var app = express();
 
 // register handlebars partials (http://www.npmjs.com/package/hbs)
